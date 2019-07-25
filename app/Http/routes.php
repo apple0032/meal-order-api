f330679dfa5efbd3bb53f4a42fb78f53abe3e73a
apple0032 <waihongip@gmail.com>
2019-07-25 22:59:44 +0800

    [Hotfix]Meal category url

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['web']], function () {
     Route::get('/meal', ['uses' => 'ApiController@getAllMeal']);
     Route::get('/meal/{id}/{userid}', ['uses' => 'ApiController@getMealById']);
-    Route::get('/meal/category/{name}', ['uses' => 'ApiController@getMealByCategory']);
+    Route::get('/meal-category/{name}', ['uses' => 'ApiController@getMealByCategory']);
     Route::post('/cart', ['uses' => 'ApiController@createCart']);
     Route::get('/cart/{user}', ['uses' => 'ApiController@getCart']);
     Route::post('/purchase', ['uses' => 'ApiController@createOrder']);
